e8c68bb11d9423bf97bdb4aced2f548b1af015d6
Lomlomm <vanessaserna60@gmail.com>
2023-06-29 21:16:59 -0600

    add logos export

--- src/assets/index.js
+++ src/assets/index.js
@@ -30,6 +30,9 @@ import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
+import agilezip from "./agilezip.png"
+import kiubix from "./kiubix.jpg"
+
 export {
   logo,
   backend,
@@ -59,4 +62,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  agilezip, 
+  kiubix,
 };
